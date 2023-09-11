0ee8ee3f2b4a78634dab3c6e299902f30c30cdc9
zibin <chenzibin2017@outlook.com>
2023-09-10 20:30:58 -0400

    syntax

---
+++ syntax.py
@@ -0,0 +1,28 @@
+
+def my_function():
+  print("Hello")
+ print("World")
+# IndentationError: unindent does not match any outer indentation level
+
+
+print("Hello, World!)
+# SyntaxError: EOL while scanning string literal
+
+
+print(("Hello, World!")
+# SyntaxError: unexpected EOF while parsing
+
+
+if True
+    print("Hello, World!")
+# SyntaxError: invalid syntax (The colon ':' is missing after 'if True')
+
+import = 5
+# SyntaxError: invalid syntax (Cannot use 'import' as a variable name because it is a keyword)
+
+x = 5
+y = 10
+print(x y)
+# SyntaxError: invalid syntax (An operator, such as + or -, is missing between x and y)
+
+
